b00949da84de6fd55e71c3501f1e37f31dfb6ae9
lqd1434 <1434288209@qq.com>
2021-11-14 22:45:05 +0800

    :zap:干掉webpack(问题太多,配置麻烦),react部分采用vite,electron部分采用parcel打包和热重载

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+// @ts-ignore
+import { chrome } from './info.json';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  build: {
+    outDir: './release/build/render',
+    emptyOutDir: true,
+    target: `chrome${chrome}`,
+    brotliSize: false
+  }
+});
