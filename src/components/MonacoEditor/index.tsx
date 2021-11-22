b25704f4205c35ab210063b59f990495a7f62e1d
lqd1434 <1434288209@qq.com>
2021-11-22 20:15:37 +0800

    fix:移除mdx依赖

--- src/components/MonacoEditor/index.tsx
+++ src/components/MonacoEditor/index.tsx
@@ -1,6 +1,8 @@
 import React, { VFC, useRef, useState, useEffect } from 'react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import styles from './index.module.scss';
+import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
+import IEditorModel = editor.IEditorModel;
 
 const Editor: VFC = () => {
   const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
@@ -8,14 +10,43 @@ const Editor: VFC = () => {
 
   useEffect(() => {
     if (monacoEl && !editor) {
+      monaco.editor.defineTheme('my-theme', {
+        base: 'vs',
+        inherit: true,
+        colors: {
+          'editor.lineHighlightBackground': '#EDEFF4'
+        },
+        rules: []
+      });
+      monaco.editor.setTheme('my-theme');
       setEditor(
         monaco.editor.create(monacoEl.current!, {
-          value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-          language: 'typescript'
+          // value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+          value: '',
+          language: 'json',
+          wordWrap: 'wordWrapColumn',
+          wordWrapColumn: 40,
+          automaticLayout: true,
+          wrappingIndent: 'indent',
+          scrollbar: {
+            verticalScrollbarSize: 10,
+            verticalSliderSize: 12
+          },
+          minimap: {
+            enabled: false
+          }
         })
       );
     }
-
+    let model = editor?.getModel();
+    model?.onDidChangeContent((e) => {
+      // console.log(e);
+      console.log(monaco.editor.getModelMarkers({}));
+      console.log(model?.getValue());
+    });
+    editor?.onDidBlurEditorWidget(() => {
+      editor?.getAction('editor.action.formatDocument').run();
+    });
     return () => editor?.dispose();
   }, [monacoEl.current]);
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import { chrome } from './info.json';
 
 export default defineConfig(async () => {
-  const mdx = (await import('@mdx-js/rollup')).default;
   return {
     base: './',
-    resolve: {
-      alias: {
-        'react/jsx-runtime': 'react/jsx-runtime.js'
-      }
-    },
-    plugins: [reactRefresh(), mdx()],
+    plugins: [reactRefresh()],
     build: {
       outDir: './release/build/render',
       emptyOutDir: true,
