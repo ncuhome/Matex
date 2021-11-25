7e7b90f59abebe3139e942004280b37287042240
lqd1434 <1434288209@qq.com>
2021-11-25 18:49:37 +0800

    feat:更改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,26 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { chrome } from './info.json';
 
-export default defineConfig(async () => {
-  return {
-    base: './',
-    publicDir: './public',
-    plugins: [reactRefresh()],
-    build: {
-      outDir: './release/build/render',
-      emptyOutDir: true,
-      target: `chrome${chrome}`,
-      brotliSize: false,
-      rollupOptions: {}
+export default defineConfig({
+  base: './',
+  publicDir: './public',
+  plugins: [reactRefresh()],
+  build: {
+    // sourcemap: true,
+    outDir: './release/build/render',
+    emptyOutDir: true,
+    target: `chrome${chrome}`,
+    brotliSize: true,
+    rollupOptions: {
+      output: {
+        compact: true,
+        assetFileNames: 'assets.[ext]',
+        chunkFileNames: 'bundle.[name].js',
+        entryFileNames: 'entity.js',
+        manualChunks: {
+          monacoEditor: ['monaco-editor']
+        }
+      }
     }
-  };
+  }
 });
