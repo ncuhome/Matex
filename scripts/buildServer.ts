a66e24b94291f44934c517ea8ece3d8d74d0da69
lqd1434 <1434288209@qq.com>
2021-12-02 15:51:25 +0800

    :zap: 删除无用文件

--- scripts/buildServer.ts
+++ scripts/buildServer.ts
@@ -5,6 +5,6 @@ console.log(__dirname);
 const dirPath = process.cwd();
 (async () => {
   execa.commandSync(
-    `cross-env NODE_ENV=production parcel build ${dirPath}/electron/server/app.ts --out-dir ${dirPath}/electron/server/dist --target node`
+    `parcel build ${dirPath}/electron/server/app.ts --out-dir ${dirPath}/electron/server/dist --target node`
   );
 })();
--- src/pages/MockView/types.ts
+++
@@ -1,6 +0,0 @@
-export interface MockData {
-  port: string;
-  route: string;
-  type: 'Plain Text' | 'JSON' | 'File' | 'Form Data';
-  data: string;
-}
