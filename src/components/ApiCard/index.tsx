531ebcccae580c83493a2e57947ef4f276ea6da6
lqd1434 <1434288209@qq.com>
2021-12-02 21:42:32 +0800

    style: ApiCard样式重构

--- src/components/ApiCard/index.tsx
+++ src/components/ApiCard/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
 import AddIcon from '@material-ui/icons/Add';
-import { myEmitter } from '../../utils/EventEmiter';
+import CircleProgress from '../CircleProgress';
+import BadgeOutlinedIcon from '@material-ui/icons/BadgeOutlined';
+import EarbudsOutlined from '@material-ui/icons/EarbudsOutlined';
+import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import { MoreHoriz } from '@material-ui/icons';
 
+const options = ['修改', '删除', '保存'];
+const ITEM_HEIGHT = 48;
 export const ApiCard = () => {
-  const onFocus = () => {
-    myEmitter.emit<boolean>('inputFocus', true);
-  };
-
-  const onBlur = () => {
-    myEmitter.emit<boolean>('inputFocus', false);
-  };
+  const [anchorEl, setAnchorEl] = useState<any>(null);
+  const open = Boolean(anchorEl);
 
   return (
     <div className={styles.apiCard}>
@@ -19,15 +20,53 @@ export const ApiCard = () => {
           <div className={styles.inner} />
         </div>
         <div className={styles.time}>2021/10/29</div>
+        <div className={styles.option}>
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls="long-menu"
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+          >
+            <MoreHoriz />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              'aria-labelledby': 'long-button'
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={() => setAnchorEl(null)}
+            PaperProps={{
+              style: {
+                maxHeight: ITEM_HEIGHT * 4.5,
+                width: '20ch'
+              }
+            }}
+          >
+            {options.map((option) => (
+              <MenuItem key={option} selected={option === '保存'} onClick={() => setAnchorEl(null)}>
+                {option}
+              </MenuItem>
+            ))}
+          </Menu>
+        </div>
       </div>
       <div className={styles.info}>
-        <div>
-          <span className={styles.label}>name:</span>
-          <input onFocus={onFocus} onBlur={onBlur} value={'test'} className={styles.content} />
+        <div className={styles.intro}>
+          <div className={styles.name}>
+            <BadgeOutlinedIcon sx={{ mr: 1 }} />
+            <span>成绩</span>
+          </div>
+          <div className={styles.port}>
+            <EarbudsOutlined sx={{ mr: 1 }} />
+            <span>8080</span>
+          </div>
         </div>
-        <div>
-          <span className={styles.label}>count:</span>
-          <input onFocus={onFocus} onBlur={onBlur} value={'test'} className={styles.content} />
+        <div className={styles.count}>
+          <CircleProgress theme={'orange'} />
         </div>
       </div>
     </div>
