d5d42a4e40481f1eef8759c548525d0780159384
lqd1434 <1434288209@qq.com>
2021-12-03 22:59:54 +0800

    feat:  完善mock表单

--- src/pages/Mock/Body/MockForm/index.tsx
+++ src/pages/Mock/Body/MockForm/index.tsx
@@ -1,8 +1,10 @@
-import React, { ChangeEvent, Fragment } from "react";
+import React, { ChangeEvent } from "react";
 import styles from "./index.module.scss";
-import Box from "@mui/material/Box";
+import LabelInput from "../../../../components/LabelInput";
+import LinkIcon from "@mui/icons-material/Link";
 
-const options = ["JSON", "Plain Text", "File", "Form Data"];
+const resOptions = ["JSON", "Plain Text", "File", "Form Data"];
+const methodsOptions = ["Get", "Post", "Put", "Delete"];
 
 const MockForm = () => {
   const [method, setMethod] = React.useState("");
@@ -13,18 +15,15 @@ const MockForm = () => {
   return (
     <div className={styles.form}>
       <div className={styles.fullUrl}>
-        <Box sx={{ minWidth: 120 }}>
-          <select className={styles.select} placeholder={'请选择类型'} onChange={handleChange}>
-            {options.map((option) => {
-              return (
-                <Fragment key={option}>
-                  <option value={option}>{option}</option>
-                </Fragment>
-              );
-            })}
-          </select>
-        </Box>
-        <input type="text" value={'http://localhost:8080/m'} className={styles.urlText} />
+        <div style={{ marginRight: 10 }}>
+          <LabelInput showLabel={false} label={"请求方法"} select options={methodsOptions} />
+        </div>
+        <input type="text" value={"http://localhost:8080/m"} className={styles.urlText} />
+      </div>
+      <div className={styles.info}>
+        <LabelInput label={"数据类型"} select options={resOptions} />
+        <LinkIcon sx={{ transform: "scale(1.5)", color: "#F86087" }} />
+        <LabelInput label={"接口路径"} />
       </div>
     </div>
   );
--- src/pages/Mock/index.tsx
+++ src/pages/Mock/index.tsx
@@ -1,59 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styles from './index.module.scss';
+import React from "react";
+import styles from "./index.module.scss";
 import APIHeader from './Header';
 import APISider from './Side';
 import APIBody from './Body';
-import { myEmitter } from '../../utils/EventEmiter';
 
 const MockView = () => {
-  const [down, setDown] = useState(false);
-  const [inputFocus, setFocus] = useState(false);
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const [startX, setStartX] = useState(0);
-
-  const onMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    e.stopPropagation();
-    window.requestAnimationFrame(() => {
-      if (down && !inputFocus) {
-        const distance = e.clientX - startX;
-        if (distance > 0) {
-          scrollRef.current!.scrollLeft -= Math.abs(distance) / 3;
-        } else {
-          scrollRef.current!.scrollLeft += Math.abs(distance) / 3;
-        }
-      }
-    });
-  };
-
-  const onMouseDown: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    e.stopPropagation();
-    setDown(true);
-    setStartX(e.clientX);
-  };
-
-  const onMouseUp: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    e.stopPropagation();
-    setDown(false);
-    setStartX(0);
-  };
-
-  useEffect(() => {
-    myEmitter.on<boolean>('inputFocus', (data) => {
-      setFocus(data);
-    });
-  }, []);
-
   return (
     <div className={styles.con}>
-      <div
-        // ref={scrollRef}
-        className={styles.header}
-        // onMouseDownCapture={onMouseDown}
-        // onMouseMove={onMouseMove}
-        // onMouseUp={onMouseUp}
-        // onMouseDown={onMouseDown}
-        // onMouseLeave={() => setDown(false)}
-      >
+      <div className={styles.header}>
         <APIHeader />
       </div>
       <div className={styles.side}>
