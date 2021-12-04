1645bf14d1d187352ca3e32fefc9a54620ea4cc4
lqd1434 <1434288209@qq.com>
2021-12-04 23:53:30 +0800

    feat: 完成启动按钮

---
+++ src/components/BallLoading/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const BallLoading = () => {
+  return (
+    <div className={styles.load}>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+    </div>
+  );
+};
+
+export default BallLoading;
--- src/pages/Mock/Side/index.tsx
+++ src/pages/Mock/Side/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { AddApiList, ApiList } from '../../../components/ApiList';
+import BallLoading from '../../../components/BallLoading';
 
 const APISider = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClick = () => {
+    setOpen(true);
+  };
+
   return (
     <div className={styles.sider}>
       <div className={styles.form}>
@@ -16,7 +25,15 @@ const APISider = () => {
         <AddApiList />
       </div>
       <div className={styles.card}>
-        <div>2</div>
+        {open ? (
+          <div onClick={handleClose} style={{ cursor: 'pointer' }}>
+            <BallLoading />
+          </div>
+        ) : (
+          <div className={styles.buttonOuter} onClick={handleClick}>
+            启动
+          </div>
+        )}
       </div>
     </div>
   );
