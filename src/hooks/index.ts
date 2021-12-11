8a9da58e8e48e4fadb8d177a018bc91ae139e7cf
lqd1434 <1434288209@qq.com>
2021-12-11 14:51:44 +0800

    feat: 新增messageChannel hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,18 @@
+import { useChannel } from '../zustand/store/apiData.store';
+import { ChannelData } from '../type/api';
+
+export const usePortChannel = <T>() => {
+  const { port } = useChannel((state) => state);
+
+  const postMessage = <T>(type: ChannelData<T>['type'], data: ChannelData<T>['data']) => {
+    try {
+      port?.postMessage({ type, data });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return {
+    postMessage
+  };
+};
