7e383c205056383add8e7a89069a7a4099d4a9ac
lqd1434 <1434288209@qq.com>
2021-12-11 13:52:17 +0800

    style: 修改启动按钮样式

--- src/pages/Mock/Side/Idle/index.tsx
+++ src/pages/Mock/Side/Idle/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { ChildType } from '../index';
+import planeIcon from '../../../../assets/icon/planefill.svg';
 
 const Idle = ({ onClick }: ChildType) => {
   return (
     <div className={styles.card}>
-      <div className={styles.buttonOuter} onClick={onClick}>
-        启动
+      <div className={styles.con}>
+        <div className={styles.imgCon}>
+          <img src={planeIcon} className={styles.plane} />
+        </div>
+        <div className={styles.buttonOuter} onClick={onClick}>
+          启动
+        </div>
       </div>
     </div>
   );
