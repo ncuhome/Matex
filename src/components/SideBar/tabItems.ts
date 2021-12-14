a9b5672517f567264fae6d84ada28fbfacda088b
lqd1434 <1434288209@qq.com>
2021-12-14 23:29:59 +0800

    style: sidebar样式修改,增加更多可用空间

--- src/components/SideBar/tabItems.ts
+++ src/components/SideBar/tabItems.ts
@@ -1,24 +1,33 @@
+import CollectIcon from '../../assets/sidarIcon/CollectIcon';
+import MockIcon from '../../assets/sidarIcon/MockIcon';
+
 export interface TabProps {
   text: string;
   route: string;
+  renderIcon: ({ active: boolean }: { active: boolean }) => JSX.Element;
   active?: boolean;
 }
 
 export const tabItems: TabProps[] = [
   {
-    text: 'Mock服务',
-    route: '/mock'
+    text: '数据获取',
+    route: '/collect',
+    renderIcon: CollectIcon
   },
+  {
+    text: 'Mock服务',
+    route: '/mock',
+    renderIcon: MockIcon
+  },
+
   {
     text: '每日排行',
-    route: '/push'
-  },
-  {
-    text: '数据获取',
-    route: '/pull'
+    route: '/push',
+    renderIcon: CollectIcon
   },
   {
     text: '敬请期待',
-    route: '/ok'
+    route: '/ok',
+    renderIcon: CollectIcon
   }
 ];
--- src/components/SideBarTab/index.tsx
+++ src/components/SideBarTab/index.tsx
@@ -4,14 +4,21 @@ import { TabProps } from '../SideBar/tabItems';
 import clsx from 'clsx';
 import { useNavigate } from 'react-router-dom';
 
-const SideBarTab: React.FC<TabProps> = ({ text = '', route = '', active }) => {
+const SideBarTab: React.FC<TabProps> = ({ text = '', route = '', active = false, renderIcon }) => {
   const navigate = useNavigate();
+  const [hover, setHover] = React.useState(false);
 
   const handleClick = () => {
     navigate(route);
   };
   return (
-    <div onClick={handleClick} className={clsx([styles.tab, active && styles.active])}>
+    <div
+      onClick={handleClick}
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      className={clsx([styles.tab, active && styles.active])}
+    >
+      <div>{renderIcon({ active: hover || active })}</div>
       {text}
     </div>
   );
