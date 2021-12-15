f83358b6ef6a596f6c802e6feab81fb568392046
lqd1434 <1434288209@qq.com>
2021-12-15 22:21:07 +0800

    fix: 修复emitter重复监听,造成的不必要render问题

--- src/components/MonacoEditor/index.tsx
+++ src/components/MonacoEditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC, useCallback, useEffect, useRef } from 'react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { languages } from 'monaco-editor/esm/vs/editor/editor.api';
 import styles from './index.module.scss';
@@ -7,8 +7,31 @@ import { resizeAble } from './resize';
 import { rules } from './theme';
 import { suggestions } from './suggestions';
 import { myEmitter } from '../../utils/EventEmiter';
+import { useEditors } from '../../zustand/store/apiData.store';
 import ProviderResult = languages.ProviderResult;
 import CompletionList = languages.CompletionList;
+
+monaco.languages.registerCompletionItemProvider('json', {
+  provideCompletionItems: (model, position, context, token) => {
+    return { suggestions: suggestions } as ProviderResult<CompletionList>;
+  }
+});
+monaco.editor.defineTheme('my-theme', {
+  base: 'vs',
+  inherit: true,
+  colors: {
+    'editor.lineHighlightBorder': '#00000000',
+    'editor.background': '#00000000',
+    'scrollbar.shadow': '#00000000',
+    'scrollbarSlider.background': '#00000000',
+    'scrollbarSlider.hoverBackground': '#00000000',
+    'scrollbarSlider.activeBackground': '#00000000',
+    'editorWidget.border': '#00000000',
+    'editorOverviewRuler.border': '#00000000'
+  },
+  rules
+});
+monaco.editor.setTheme('my-theme');
 
 interface MonacoEditorProps {
   name: string;
@@ -45,73 +68,63 @@ const MonacoEditor: FC<MonacoEditorProps> = ({
   setValue = () => {},
   onFocus = () => {}
 }) => {
-  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
   const monacoEl = useRef(null);
-
+  const { editors, addEditor, deleteEditor } = useEditors((state) => state);
   useEffect(() => {
     resizeAble();
   }, [monacoEl]);
+  const editor = editors.get(name);
+  console.log('editor', editor);
+  console.log(editors);
 
   useEffect(() => {
-    myEmitter.on<string>(`monacoEditor-${name}`, (val) => {
-      if (val) {
+    console.log('重新render');
+  }, [editor]);
+
+  const setVal = useCallback(
+    (value: string) => {
+      if (value) {
         console.log(editor);
-        editor?.setValue(val);
+        console.log(myEmitter.listeners);
+        editor?.setValue(value);
         editor?.getAction('editor.action.formatDocument')?.run();
       }
-    });
-  }, [editor]);
+    },
+    [editor]
+  );
 
   useEffect(() => {
-    monaco.editor.defineTheme('my-theme', {
-      base: 'vs',
-      inherit: true,
-      colors: {
-        'editor.lineHighlightBorder': '#00000000',
-        'editor.background': '#00000000',
-        'scrollbar.shadow': '#00000000',
-        'scrollbarSlider.background': '#00000000',
-        'scrollbarSlider.hoverBackground': '#00000000',
-        'scrollbarSlider.activeBackground': '#00000000',
-        'editorWidget.border': '#00000000',
-        'editorOverviewRuler.border': '#00000000'
-      },
-      rules
-    });
-    monaco.editor.setTheme('my-theme');
-  }, [editor]);
-  useEffect(() => {
-    setEditor(null);
-    monaco.languages.registerCompletionItemProvider('json', {
-      provideCompletionItems: (model, position, context, token) => {
-        return { suggestions: suggestions } as ProviderResult<CompletionList>;
-      }
-    });
     if (monacoEl && !editor) {
       console.log('初始化---');
-      setEditor(
-        monaco.editor.create(monacoEl.current!, {
-          value: defaultVal,
-          language,
-          readOnly,
-          automaticLayout: true,
-          fontSize: 14,
-          fontFamily: 'JetBrains Mono NL Light',
-          scrollbar: {
-            verticalScrollbarSize: 10,
-            verticalSliderSize: 12
-          },
-          minimap: {
-            enabled: enabledMinMap
-          }
-        })
-      );
+      const editor_ = monaco.editor.create(monacoEl.current!, {
+        value: defaultVal,
+        language,
+        readOnly,
+        automaticLayout: true,
+        fontSize: 14,
+        fontFamily: 'JetBrains Mono NL Light',
+        scrollbar: {
+          verticalScrollbarSize: 10,
+          verticalSliderSize: 12
+        },
+        minimap: {
+          enabled: enabledMinMap
+        }
+      });
+      addEditor(name, editor_);
     }
     return () => {
-      console.log('删除----');
-      editor?.dispose();
+      console.log('清除editor');
+      deleteEditor(name);
+      myEmitter.offAll(`monacoEditor-${name}`);
     };
-  }, [monacoEl.current]);
+  }, []);
+
+  useEffect(() => {
+    if (editor) {
+      myEmitter.on<string>(`monacoEditor-${name}`, setVal);
+    }
+  }, [editor]);
 
   useEffect(() => {
     let model = editor?.getModel();
@@ -123,12 +136,10 @@ const MonacoEditor: FC<MonacoEditorProps> = ({
     });
     editor?.onDidBlurEditorText(() => {
       onBlur();
-      console.log('失去焦点');
       autoFormat && editor?.getAction('editor.action.formatDocument').run();
     });
     editor?.onDidBlurEditorWidget(() => {
       onBlur();
-      console.log('失去焦点-onDidBlurEditorWidget');
       autoFormat && editor?.getAction('editor.action.formatDocument').run();
     });
     editor?.onDidFocusEditorWidget(() => {
