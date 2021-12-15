9ffaedb08d3a5afbcf9f164e3b930adc9abc5652
lqd1434 <1434288209@qq.com>
2021-12-15 16:48:24 +0800

    feat: 为loading页面增加复制脚本

--- scripts/copyFile.ts
+++ scripts/copyFile.ts
@@ -1,15 +1,16 @@
-import { copyFileSync, existsSync } from 'fs-extra';
+import { copyFileSync } from 'fs-extra';
 import path from 'path';
 import signale from 'signale';
 
-const buildPath = path.resolve(process.cwd(), './release/build/main/preload.js');
+const buildPath1 = path.resolve(process.cwd(), './release/build/main/preload.js');
 const preloadPath = path.resolve(process.cwd(), './electron/scripts/preload.js');
-
-if (!existsSync(buildPath)) {
-  try {
-    copyFileSync(preloadPath, buildPath);
-    signale.success('复制 preload.js 到 build/main 成功');
-  } catch (e) {
-    signale.error('复制失败');
-  }
+const buildPath2 = path.resolve(process.cwd(), './release/build/render/loading.html');
+const loadingPath = path.resolve(process.cwd(), './loading.html');
+try {
+  copyFileSync(preloadPath, buildPath1);
+  signale.success('复制 preload.js 到 build/main 成功');
+  copyFileSync(loadingPath, buildPath2);
+  signale.success('复制 loading.html 到 build/render 成功');
+} catch (e) {
+  signale.error('复制失败');
 }
