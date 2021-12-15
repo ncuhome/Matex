c4b0ace937604e27148c860613f8920004aabbb7
lqd1434 <1434288209@qq.com>
2021-12-15 14:09:15 +0800

    feat: 从主进程为渲染进程注入axios对象

--- electron/scripts/preload.js
+++ electron/scripts/preload.js
@@ -2,9 +2,23 @@ window.addEventListener('DOMContentLoaded', () => {
   const inject = () => {
     const { ipcRenderer } = require('electron');
     window.ipc = ipcRenderer;
+    injectAxios();
   };
 
   for (const type of ['chrome', 'node', 'electron']) {
     inject();
   }
 });
+
+const injectAxios = () => {
+  const axios = require('axios');
+  const signale = require('signale');
+  axios.interceptors.request.use(
+    (config) => {
+      signale.debug(config);
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+  window.Mixos = axios;
+};
