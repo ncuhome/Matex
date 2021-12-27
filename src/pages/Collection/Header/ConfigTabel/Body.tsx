3fd5e64b5154caa2c4852b6e323183b719c8cb8b
lqd1434 <1434288209@qq.com>
2021-12-28 00:33:43 +0800

    fix: fix render fewer hooks error in body

--- src/pages/Collection/Header/ConfigTabel/Body.tsx
+++ src/pages/Collection/Header/ConfigTabel/Body.tsx
@@ -1,5 +1,5 @@
 import { Button, Icon, Popup, Segment, Table } from 'semantic-ui-react';
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useMemo } from 'react';
 import { useBodyList, useUrlConfig } from '../../../../zustand/store/collection.store';
 import styles from './index.module.scss';
 import { BodyTypes } from '../../../../Model/collection.model';
@@ -21,8 +21,14 @@ const BodyTable = () => {
   if (method === 'Get') {
     return (
       <Segment style={{ marginTop: 10 }} color="red" textAlign={'center'}>
-        <Icon name={'warning sign'} color={'red'} />
-        <span style={{ color: 'red' }}>&nbsp;当为Get请求时,无此配置</span>
+        {useMemo(() => {
+          return (
+            <div>
+              <Icon name={'warning sign'} color={'red'} />
+              <span style={{ color: 'red' }}>&nbsp;当为Get请求时,无此配置</span>
+            </div>
+          );
+        }, [bodyList, method])}
       </Segment>
     );
   } else {
@@ -37,65 +43,73 @@ const BodyTable = () => {
             </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-
-        <Table.Body>
-          {bodyList.map((item, index) => {
-            return (
-              <Fragment key={item.index}>
-                <Table.Row>
-                  <Table.Cell textAlign={'center'}>
-                    <input
-                      className={styles.input}
-                      value={item.key}
-                      onChange={(e) => {
-                        console.log('change');
-                        updateBody(index, 'key', e.target.value);
-                      }}
-                    />
-                  </Table.Cell>
-                  <Table.Cell textAlign={'center'}>
-                    <div className={styles.bodyCell}>
-                      <input
-                        className={styles.input}
-                        value={item.value}
-                        onChange={(e) => {
-                          updateBody(index, 'value', e.target.value);
-                        }}
-                      />
-                      <Button.Group basic size="mini">
-                        <Button icon="eye" disabled />
-                        <Popup position="bottom center" on={'click'} trigger={<Button icon="file" />} flowing>
-                          <Button.Group vertical>
-                            {BodyTypes.map((item) => {
-                              return (
-                                <Fragment key={item}>
-                                  <Button>
-                                    <Icon name="file" />
-                                    {item}
-                                  </Button>
-                                </Fragment>
-                              );
-                            })}
+        {useMemo(() => {
+          return (
+            <Table.Body>
+              {bodyList.map((item, index) => {
+                return (
+                  <Fragment key={item.index}>
+                    <Table.Row>
+                      <Table.Cell textAlign={'center'}>
+                        <input
+                          className={styles.input}
+                          value={item.key}
+                          onChange={(e) => {
+                            console.log('change');
+                            updateBody(index, 'key', e.target.value);
+                          }}
+                        />
+                      </Table.Cell>
+                      <Table.Cell textAlign={'center'}>
+                        <div className={styles.bodyCell}>
+                          <input
+                            className={styles.input}
+                            value={item.value}
+                            onChange={(e) => {
+                              updateBody(index, 'value', e.target.value);
+                            }}
+                          />
+                          <Button.Group basic size="mini">
+                            <Button icon="eye" disabled />
+                            <Popup
+                              position="bottom center"
+                              on={'click'}
+                              trigger={<Button icon="file" />}
+                              flowing
+                            >
+                              <Button.Group vertical>
+                                {BodyTypes.map((item) => {
+                                  return (
+                                    <Fragment key={item}>
+                                      <Button>
+                                        <Icon name="file" />
+                                        {item}
+                                      </Button>
+                                    </Fragment>
+                                  );
+                                })}
+                              </Button.Group>
+                            </Popup>
                           </Button.Group>
-                        </Popup>
-                      </Button.Group>
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell textAlign={'center'}>
-                    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                      <Button icon compact>
-                        <Icon name="expand arrows alternate" />
-                      </Button>
-                      <Button icon compact onClick={() => deleteBody(item.index)}>
-                        <Icon name="delete" />
-                      </Button>
-                    </div>
-                  </Table.Cell>
-                </Table.Row>
-              </Fragment>
-            );
-          })}
-        </Table.Body>
+                        </div>
+                      </Table.Cell>
+                      <Table.Cell textAlign={'center'}>
+                        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                          <Button icon compact>
+                            <Icon name="expand arrows alternate" />
+                          </Button>
+                          <Button icon compact onClick={() => deleteBody(item.index)}>
+                            <Icon name="delete" />
+                          </Button>
+                        </div>
+                      </Table.Cell>
+                    </Table.Row>
+                  </Fragment>
+                );
+              })}
+            </Table.Body>
+          );
+        }, [bodyList, method])}
       </Table>
     );
   }
