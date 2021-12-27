ab706adf54602141616ec0a850bc56d1dc83e4b1
lqd1434 <1434288209@qq.com>
2021-12-27 16:56:23 +0800

    style: 完善tab样式

--- src/pages/Collection/Header/Tabs/index.tsx
+++ src/pages/Collection/Header/Tabs/index.tsx
@@ -1,8 +1,9 @@
 import React, { useMemo } from 'react';
-import { Label, Menu } from 'semantic-ui-react';
+import { Button, Dropdown, Label, Menu } from 'semantic-ui-react';
 import { TabItems } from '../../../../type/collection';
 import { useUrlConfig } from '../../../../zustand/store/collection.store';
 import { useNavigate } from 'react-router-dom';
+import { MethodsOptions } from '../../../../Model/collection.model';
 
 const Tabs = () => {
   const { activeTab, setActiveTab } = useUrlConfig((state) => state);
@@ -13,6 +14,9 @@ const Tabs = () => {
     const path = `/collect/${item.toLowerCase()}`;
     navigate(path);
   };
+  const countryOptions = MethodsOptions.map((item) => {
+    return { key: item, value: item, text: item };
+  });
 
   return useMemo(() => {
     return (
@@ -20,7 +24,7 @@ const Tabs = () => {
         <Label ribbon as="a" color={'orange'}>
           选项
         </Label>
-        <Menu color={'teal'} secondary style={{ marginBottom: 10 }}>
+        <Menu color={'teal'} secondary fluid>
           <Menu.Item
             name="Params"
             active={activeTab === 'Params'}
@@ -31,7 +35,17 @@ const Tabs = () => {
             active={activeTab === 'Headers'}
             onClick={() => handleItemClick('Headers')}
           />
-          <Menu.Item name="Body" active={activeTab === 'Body'} onClick={() => handleItemClick('Body')} />
+          <Menu.Item name={'Body'} active={activeTab === 'Body'} onClick={() => handleItemClick('Body')} />
+          {activeTab === 'Body' && (
+            <Menu.Menu position="right" style={{ marginRight: 15 }}>
+              <Menu.Item>
+                <Button.Group color={'blue'}>
+                  <Button>数据类型</Button>
+                  <Dropdown className="button icon" options={countryOptions} trigger={<></>} />
+                </Button.Group>
+              </Menu.Item>
+            </Menu.Menu>
+          )}
         </Menu>
       </>
     );
