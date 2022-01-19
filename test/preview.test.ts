46688cae1f85f234d1631010f0e3b4f5d1eee4a6
lqd1434 <1434288209@qq.com>
2022-01-20 01:45:53 +0800

    update: 修改重构以后的preview测试逻辑

---
+++ test/preview.test.ts
@@ -0,0 +1,66 @@
+import WaitOn from 'wait-on';
+import { exec } from 'child_process';
+import Signale from 'signale';
+import { chromium } from 'playwright';
+
+const options: WaitOn.WaitOnOptions = {
+  resources: ['tcp:5000'],
+  delay: 1000, // initial delay in ms, default 0
+  interval: 100, // poll interval in ms, default 250ms
+  simultaneous: 1, // limit to 1 connection per resource at a time
+  timeout: 30000, // timeout in ms, default Infinity
+  tcpTimeout: 1000, // tcp timeout in ms, default 300ms
+  window: 1000
+};
+
+(async () => {
+  const preview = exec('npm run preview');
+
+  preview.stdout?.on('data', (message: string) => {
+    Signale.log(message);
+  });
+
+  preview.stderr?.on('data', (message: string) => {
+    Signale.error(message);
+  });
+  //启动截屏
+  try {
+    await WaitOn(options);
+    const browser = await chromium.launch({
+      headless: false,
+      devtools: false
+    });
+    const context = await browser.newContext();
+    // Open new page
+    const page = await context.newPage();
+    // Go to http://localhost:3000/
+    await page.goto('http://localhost:5000/');
+    // Go to http://localhost:3000/login
+    await page.goto('http://localhost:5000/login');
+    // Click text=登录页面
+    await page.click('text=登录页面');
+    // assert.equal(page.url(), 'http://localhost:3000/collect');
+    // Click text=Body
+    await page.click('text=Body');
+    await page.screenshot({ path: './test/screenshot/page_collection.png' });
+    // Click text=Headers
+    await page.click('text=Headers');
+    // Click text=虚拟接口
+    await page.click('text=虚拟接口');
+    await page.screenshot({ path: './test/screenshot/page_mock.png' });
+    // assert.equal(page.url(), 'http://localhost:3000/mock');
+    // Click text=压力测试
+    await page.click('text=压力测试');
+    await page.screenshot({ path: './test/screenshot/benchmark.png' });
+    // assert.equal(page.url(), 'http://localhost:3000/benchmark');
+    // Click text=敬请期待
+    await page.click('text=敬请期待');
+    // assert.equal(page.url(), 'http://localhost:3000/ok');
+    // ---------------------
+    await context.close();
+    await browser.close();
+    process.exit(0);
+  } catch (e) {
+    Signale.error(e);
+  }
+})();
