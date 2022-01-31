e88aabad1c6f238070a11f0fca64f476fdb38571
lqd1434 <1434288209@qq.com>
2022-02-01 02:22:02 +0800

    :zap: 缩短部分状态码对应描述

--- electron/src/utils/statusCodeMapping.ts
+++ electron/src/utils/statusCodeMapping.ts
@@ -1,13 +1,13 @@
 const statusCodeMapping = new Map<number, string>([
   [100, 'Continue'],
-  [101, 'Switching Protocols'],
+  [101, 'Switch Protocols'],
   [102, 'Processing'],
   [103, 'Early Hints'],
   //success
   [200, 'OK'],
   [201, 'Created'],
   [202, 'Accepted'],
-  [203, 'Non-Authoritative Information'],
+  [203, 'Non-Auth Info'],
   [204, 'No Content'],
   [205, 'Reset Content'],
   [206, 'Partial Content'],
@@ -29,7 +29,7 @@ const statusCodeMapping = new Map<number, string>([
   [404, 'Not Found'],
   [405, 'Method Not Allowed'],
   [406, 'Not Acceptable'],
-  [407, 'Proxy Authentication Required'],
+  [407, 'Proxy Auth'],
   [408, 'Request Timeout'],
   [409, 'Conflict'],
   [410, 'Gone'],
@@ -39,9 +39,7 @@ const statusCodeMapping = new Map<number, string>([
   [502, 'Bad Gateway'],
   [503, 'Service Unavailable'],
   [504, 'Gateway Timeout'],
-  [505, 'HTTP Version Not'],
-  [507, 'HTTP Version Not Supported'],
-  [508, 'Loop Detected']
+  [505, 'HTTP Version']
 ]);
 
 export const getDescription = (statusCode: number) => {
