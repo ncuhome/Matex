eb8dcf4cd2b63bf2501b47e2deff935755d5e1dc
lqd1434 <1434288209@qq.com>
2022-01-31 18:23:50 +0800

    feat: 响应类型完善

--- common/index.d.ts
+++ common/index.d.ts
@@ -1,4 +1,5 @@
 import type { IpcRenderer } from 'electron';
+import type { IncomingHttpHeaders } from 'http';
 
 export interface NodeApiProps {
   NODE_ENV: 'development' | 'production' | string;
@@ -15,5 +16,23 @@ export interface ApiTestReqProps {
   };
   params: {
     [key: string]: string;
+  };
+}
+
+export interface ApiTestResProps<T = any> {
+  type: 'html' | 'text' | 'xml' | 'image' | 'audio' | 'video' | 'json' | 'pdf' | 'word' | 'bytes' | 'other';
+  statusCode: number;
+  desc: string;
+  body: T;
+  headers: IncomingHttpHeaders;
+  timer: {
+    wait?: number | undefined;
+    dns?: number | undefined;
+    tcp?: number | undefined;
+    tls?: number | undefined;
+    request?: number | undefined;
+    firstByte?: number | undefined;
+    download?: number | undefined;
+    total?: number | undefined;
   };
 }
