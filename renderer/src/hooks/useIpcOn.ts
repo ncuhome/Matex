48bc3dc44867c9bef58339572cd75c549f3ddf2d
lqd1434 <1434288209@qq.com>
2022-02-03 17:50:18 +0800

    fix: 修复ipc hooks listener函数里有组件状态逻辑造成的状态值一直为初始值的问题

--- renderer/src/hooks/useIpcOn.ts
+++ renderer/src/hooks/useIpcOn.ts
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { MatexWin } from '../global';
 import type { IpcRendererEvent } from 'electron';
 
 export type IpcListener = (e: IpcRendererEvent, msg?: any) => void;
 
 const useIpcOn = (channel: string, listener: IpcListener) => {
+  const listenerRef = useRef<IpcListener>(listener);
   useEffect(() => {
+    console.log('listener改变');
+    MatexWin.ipc?.removeListener(channel, listenerRef.current);
+    listenerRef.current = listener;
     MatexWin.ipc?.on(channel, listener);
     return () => {
       MatexWin.ipc?.removeListener(channel, listener);
     };
-  }, []);
+  }, [listener]);
 
   const off = () => {
     MatexWin.ipc?.removeListener(channel, listener);
