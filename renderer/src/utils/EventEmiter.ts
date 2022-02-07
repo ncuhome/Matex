c71b7a69e5c94793585a3a7e54ad90b379c6dbaa
lqd1434 <1434288209@qq.com>
2022-02-07 19:10:26 +0800

    feat: 缓存最后一次emit的值,在on的时候调用传递

--- renderer/src/utils/EventEmiter.ts
+++ renderer/src/utils/EventEmiter.ts
@@ -1,86 +1,27 @@
-interface Listener {
-  name: string;
-  callback: ((data: any) => void)[];
-}
+import Emittery from 'emittery';
 
-class MyEventEmitter {
-  listeners: Listener[] = [];
+const emitter = new Emittery();
 
-  /**
-   * 可以有多个监听者,返回插入的监听对象位置和插入的监听函数位置,用以之后的取消订阅
-   * 若要取消监听,回调函数应该是同一个引用
-   * @param name 监听命名空间
-   * @param callback 回调 可用泛型进一步约束
-   */
-  on<T extends any>(name: string, callback: (data: T) => void) {
-    let exist: boolean = false;
-    let itemIndex: number = -1;
-    let funcIndex: number;
-    this.listeners.forEach((item, index) => {
-      if (item.name === name) {
-        exist = true;
-        itemIndex = index;
-      }
-    });
-    //如果不存在,则创建一个
-    if (!exist) {
-      itemIndex = this.listeners.push({ name: name, callback: [callback] }) - 1;
-      funcIndex = 0;
-    } else {
-      //否则新增一个监听
-      funcIndex = this.listeners[itemIndex].callback.push(callback) - 1;
+export default class MyEmitter {
+  caches: Map<string, any>;
+
+  constructor() {
+    this.caches = new Map();
+  }
+
+  on(eventName: string, callback: (value: any) => void) {
+    const cache = this.caches.get(eventName);
+    if (cache) {
+      callback(cache);
+      this.caches.delete(eventName);
     }
-    return [itemIndex, funcIndex];
+    return emitter.on(eventName, callback);
   }
 
-  /**
-   * 可以有多个监听者,返回插入的监听对象位置和插入的监听函数位置,用以之后的取消订阅
-   * 若要取消监听,回调函数应该是同一个引用
-   * @param name 监听命名空间
-   * @param callback 回调
-   */
-  once<T extends any>(name: string, callback: (data: T) => void) {
-    const only = (data: any) => {
-      callback(data);
-      this.off(name, only);
-    };
-    this.on(name, only);
-  }
-
-  /**
-   * 可以有多个监听者,返回插入的监听对象位置和插入的监听函数位置,用以之后的取消订阅
-   * 若要取消监听,回调函数应该是同一个引用
-   * @param name 监听命名空间
-   * @param data 要传输的数据
-   */
-  emit<T extends any>(name: string, data: T) {
-    this.listeners.forEach((item, index) => {
-      if (item.name === name) {
-        //向所有监听者发送
-        this.listeners[index].callback.forEach((fn) => fn(data));
-      }
-    });
-  }
-
-  off(name: string, callback: (data: any) => void) {
-    this.listeners.forEach((item, index) => {
-      if (item.name === name) {
-        this.listeners[index].callback.forEach((fn, index2) => {
-          if (fn === callback) {
-            this.listeners[index].callback.splice(index2, 1);
-          }
-        });
-      }
-    });
-  }
-
-  //移除某个命名空间下的所有监听者
-  offAll(name: string) {
-    this.listeners.forEach((item, index) => {
-      if (item.name === name) {
-        this.listeners[index].callback = [];
-      }
-    });
+  emit(eventName: string, value: any) {
+    this.caches.set(eventName, value);
+    emitter.emit(eventName, value);
   }
 }
-export const myEmitter = new MyEventEmitter();
+
+export const Emitter = new MyEmitter();
