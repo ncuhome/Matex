391bd1fdb690ae75e23307d4c8fd7c28257b3d3d
lqd1434 <1434288209@qq.com>
2022-02-18 20:37:17 +0800

    feat: add logo

--- renderer/src/components/Header/index.tsx
+++ renderer/src/components/Header/index.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import styles from './index.module.scss';
-import rainbow from '../../assets/icon/rainbow.svg';
-import { useAtom } from 'jotai';
-import { collapseAtom } from '../../store/commonStore';
 
 const Header = () => {
-  const [collapse, setCollapse] = useAtom(collapseAtom);
-  const handleClick = () => {
-    if (!collapse) {
-      setCollapse(true);
-    } else {
-      setCollapse(false);
-    }
-  };
   return (
     <div className={styles.header}>
-      <div
-        className={styles.logoCon}
-        style={{ marginLeft: collapse ? '-16px' : '35px' }}
-        onClick={handleClick}
-      >
-        <img src={rainbow} className={styles.logo} alt={''} />
-      </div>
+      {/*<div*/}
+      {/*  className={styles.logoCon}*/}
+      {/*  style={{ marginLeft: collapse ? '-16px' : '35px' }}*/}
+      {/*  onClick={handleClick}*/}
+      {/*>*/}
+      {/*  <img src={rainbow} className={styles.logo} alt={''} />*/}
+      {/*</div>*/}
       {/*<h2 className={styles.title}>接口测试</h2>*/}
     </div>
   );
--- renderer/src/components/SideBar/index.tsx
+++ renderer/src/components/SideBar/index.tsx
@@ -3,19 +3,37 @@ import styles from './index.module.scss';
 import Tab from '../SideBarTab';
 import { sidebarModel } from '/@/model/sidebar.model';
 import { useLocation } from 'react-router-dom';
+import logo from '/@/assets/image/logo.png';
+import { useAtom } from 'jotai';
+import { collapseAtom } from '/@/store/commonStore';
 
 const SideBar = () => {
   const location = useLocation();
+  const [collapse, setCollapse] = useAtom(collapseAtom);
+  const handleClick = () => {
+    if (!collapse) {
+      setCollapse(true);
+    } else {
+      setCollapse(false);
+    }
+  };
+
   return (
-    <div className={styles.sideBar}>
-      {sidebarModel.map(({ route, text }, index) => {
-        return (
-          <Fragment key={text}>
-            <Tab text={text} route={route} active={location.pathname.startsWith(route)} />
-          </Fragment>
-        );
-      })}
-    </div>
+    <>
+      <div className={styles.sideBar}>
+        <div className={styles.title} onClick={handleClick}>
+          <img src={logo} className={styles.logo} alt={'logo'} />
+          {!collapse && <h3 className={styles.titleText}>Matex</h3>}
+        </div>
+        {sidebarModel.map(({ route, text }) => {
+          return (
+            <Fragment key={text}>
+              <Tab text={text} route={route} active={location.pathname.startsWith(route)} />
+            </Fragment>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
