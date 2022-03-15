2531a59432aced81d9e99042ef5c88f9add9ebd2
lqd1434 <1434288209@qq.com>
2022-03-16 00:23:43 +0800

    feat: 上传打包产物

--- scripts/generateZip.js
+++ scripts/generateZip.js
@@ -2,13 +2,52 @@ import AdmZip from 'adm-zip';
 import os from 'os';
 import { resolve } from 'path';
 import { ColorLog } from './colorLog.js';
+import fs from 'fs';
+import {promisify} from 'util';
+import matexhttp from 'matexhttp';
+const ReqAsync = promisify(matexhttp);
 
+
+const server = process.argv[2];
 (async () => {
   if (os.platform() === 'darwin') {
     const dir = resolve(process.cwd(), './release/build/mac/Matex.app/Contents/Resources/app');
     const zip = new AdmZip();
     zip.addLocalFolder(dir, 'app');
-    zip.writeZip(resolve(process.cwd(), './release/build/zip/app.zip'));
-    ColorLog.success('❤ 创建zip成功 ❤');
+    const zipPath = resolve(process.cwd(), './release/build/zip/mac.zip');
+    zip.writeZip(zipPath,async (err)=>{
+      if (err) {
+        ColorLog.error('❤ win创建zip失败 ❤');
+      } else {
+        ColorLog.success('❤ win创建zip成功 ❤');
+        const response = await ReqAsync({
+          url:`${server}/update/uploadMac`,
+          method: 'POST',
+          body: fs.readFileSync(zipPath)
+        });
+        console.log(response.strBody);
+      }
+    });
+    ColorLog.success('❤ mac创建zip成功 ❤');
+  } else if(os.platform() === 'win32') {
+    const dir = resolve(process.cwd(), './release/build/win-unpacked/resources/app');
+    const zip = new AdmZip();
+    zip.addLocalFolder(dir, 'app');
+    const zipPath = resolve(process.cwd(), './release/build/zip/win.zip');
+    await zip.writeZip(zipPath,async (err) => {
+      if (err) {
+        ColorLog.error('❤ win创建zip失败 ❤');
+      } else {
+        ColorLog.success('❤ win创建zip成功 ❤');
+        const response = await ReqAsync({
+          url:`${server}/update/uploadWin`,
+          method: 'POST',
+          body: fs.readFileSync(zipPath)
+        });
+        console.log(response.strBody);
+      }
+    });
+  } else {
+    ColorLog.error('❤ 创建zip失败 ❤');
   }
 })();
